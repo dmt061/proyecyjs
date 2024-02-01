5d16e0c4d531e449184682f1e494f28bc00b6339
dmt061 <158508858+dmt061@users.noreply.github.com>
2024-02-01 14:29:44 -0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,17 @@
+console.log("Hola Mundo")   
+
+// podemos usar variables reservadas, var , ley //
+
+var name= "dmt";
+
+let age= 17;
+
+const pi = 3.1416 
+
+console.log("mi nombre es:",name, " Y mi edad es", age, );
+
+let note= 4.5 
+let fruits=['manzana','peras'];
+console.log(fruits)
+
+let isActive= true;
